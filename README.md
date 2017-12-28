110d7ab09c6610f7312d0ceef4adf89c3db6962d
Soto <soto.davidj@gmail.com>
2017-12-27 18:57:12 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # secret-typed-pet
 Type a animal type and watch the dev console easter egg.
+
+Launch the [app](https://sotoxp.github.io/secret-typed-pet/).
---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Guess the pet by typing ie: bird, hamster, etc');
+
+const pressed = [];
+const secretCode = 'cat';
+
+window.addEventListener('keyup', (e) => {
+	console.log(e.key);
+	pressed.push(e.key);
+	pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+	if(pressed.join('').includes(secretCode)) {
+		console.log('DING DING!');
+		cornify_add();
+	}
+	console.log(pressed);
+})
